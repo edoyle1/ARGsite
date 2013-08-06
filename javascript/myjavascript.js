63ebeee7dc1b636cdd35bc4bb369728dec2294b5
sgrimshaw <sharon.grimshaw@students.olin.edu>
2013-08-06 11:11:32 -0400

    comm solutions table/border, sidebar fixes (active and scrolling)

--- javascript/myjavascript.js
+++ javascript/myjavascript.js
@@ -1,7 +1,13 @@
 // function definition
-myFun = function() {
-
-};
+// myFun = function() {};
 
 // bind an action (a function) to an event
-$("li").hover(myFun);
+// $("li").hover(myFun);
+/*
+window.onscroll = function(){}
+	var sticky = document.getElementById('stickynav');
+    //if(document.body.scrollTop+document.documentElement.scrollTop > 240)
+    sticky.className = "stuck";
+    //else sticky.className = "";
+};
+*/
