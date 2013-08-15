9ae0c97560215018a3844bfb1992dd336d5fe731
Elizabeth Doyle <penguinbar2002@gmail.com>
2013-08-15 15:58:41 -0400

    nitpicking fixes, maybe some other, pub. broken
    trying to fix scrolling issue with javascript, so publications page the
    sidbar is currently fixed

--- javascript/myjavascript.js
+++ javascript/myjavascript.js
@@ -3,11 +3,17 @@
 
 // bind an action (a function) to an event
 // $("li").hover(myFun);
-/*
-window.onscroll = function(){}
-	var sticky = document.getElementById('stickynav');
-    //if(document.body.scrollTop+document.documentElement.scrollTop > 240)
-    sticky.className = "stuck";
-    //else sticky.className = "";
-};
-*/
+
+$(document).ready(function(){
+    var names = ['#sidebar'];
+    for (var i = 0; i < names.length; i++){
+        (function(i){
+            var menuYloc = null;
+            menuYloc = parseInt($(names[i]).css('top').substring(0,$(names[i]).css('top').indexOf('px')));
+            $(window).scroll(function(){
+                var offset = menuYloc + $(document).scrollTop() + parseInt($('#header').css('height').substring(0,$('#header').css('height').indexOf('px'))) + 'px';
+                $(names[i]).animate({top:offset},{duration:0,queue:false});
+            });
+        })(i);
+    };
+});
