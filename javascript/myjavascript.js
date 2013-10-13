e16933822889c5b4c7c93d0b1481627cdb6d848c
Elizabeth Doyle <penguinbar2002@gmail.com>
2013-10-12 20:19:15 -0400

    tiny fixes  -boxes on pics, pics updated, etc
    pub/press titles updated, people=researchers on ppl page, contact us
    formatted, other small changes

--- javascript/myjavascript.js
+++ javascript/myjavascript.js
@@ -5,15 +5,32 @@
 // $("li").hover(myFun);
 
 $(document).ready(function(){
-    var names = ['#sidebar'];
+                floatObj()
+                var height = $('.row-fluid').position().top+$('.row-fluid').outerHeight(true) + 30
+                $('#floatMenu').css({top:height});
+    $(window).resize(function() {
+                var curr = $('#floatMenu').position().top
+                var height = $('.row-fluid').position().top+$('.row-fluid').outerHeight(true) + 30
+                                if ($(document).scrollTop() > height){
+                                                $('#floatMenu').animate({top:$(document).scrollTop()},{duration:0,queue:false});}
+                                else{
+                                                $('#floatMenu').animate({top:height + $(document).scrollTop()},{duration:0,queue:false});
+                                }
+    });
+});
+
+// Floating NavBar - Side
+function floatObj()
+{
+    var names = ['#floatMenu'];
     for (var i = 0; i < names.length; i++){
         (function(i){
-            var menuYloc = null;
-            menuYloc = parseInt($(names[i]).css('top').substring(0,$(names[i]).css('top').indexOf('px')));
             $(window).scroll(function(){
-                var offset = menuYloc + $(document).scrollTop() + parseInt($('#header').css('height').substring(0,$('#header').css('height').indexOf('px'))) + 'px';
-                $(names[i]).animate({top:offset},{duration:0,queue:false});
-            });
+                        var menuYloc = $('.row-fluid').position().top+$('.row-fluid').outerHeight(true) + 30;
+                var offset = $(document).scrollTop() + 20 + 'px';
+                if ($(document).scrollTop() > menuYloc){
+                                $(names[i]).animate({top:offset},{duration:0,queue:false});}
+                });
         })(i);
     };
-});
+};
