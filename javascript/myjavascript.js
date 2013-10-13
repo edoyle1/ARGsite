e51f4eef7ced75e4f6a109db4eac4819da6f8e8a
sgrimshaw <sharon.grimshaw@students.olin.edu>
2013-10-13 15:47:45 -0400

    fixing more merge conflicts and reformatting code

--- javascript/myjavascript.js
+++ javascript/myjavascript.js
@@ -5,32 +5,32 @@
 // $("li").hover(myFun);
 
 $(document).ready(function(){
-                floatObj()
-                var height = $('.row-fluid').position().top+$('.row-fluid').outerHeight(true) + 30
-                $('#floatMenu').css({top:height});
-    $(window).resize(function() {
-                var curr = $('#floatMenu').position().top
-                var height = $('.row-fluid').position().top+$('.row-fluid').outerHeight(true) + 30
-                                if ($(document).scrollTop() > height){
-                                                $('#floatMenu').animate({top:$(document).scrollTop()},{duration:0,queue:false});}
-                                else{
-                                                $('#floatMenu').animate({top:height + $(document).scrollTop()},{duration:0,queue:false});
-                                }
-    });
+	floatObj()
+	var height = $('.row-fluid').position().top+$('.row-fluid').outerHeight(true) + 30
+	$('#floatMenu').css({top:height});
+	$(window).resize(function() {
+		var curr = $('#floatMenu').position().top
+		var height = $('.row-fluid').position().top+$('.row-fluid').outerHeight(true) + 30
+		if ($(document).scrollTop() > height){
+			$('#floatMenu').animate({top:$(document).scrollTop()},{duration:0,queue:false});}
+			else{
+				$('#floatMenu').animate({top:height + $(document).scrollTop()},{duration:0,queue:false});
+			}
+		});
 });
 
 // Floating NavBar - Side
 function floatObj()
 {
-    var names = ['#floatMenu'];
-    for (var i = 0; i < names.length; i++){
-        (function(i){
-            $(window).scroll(function(){
-                        var menuYloc = $('.row-fluid').position().top+$('.row-fluid').outerHeight(true) + 30;
-                var offset = $(document).scrollTop() + 20 + 'px';
-                if ($(document).scrollTop() > menuYloc){
-                                $(names[i]).animate({top:offset},{duration:0,queue:false});}
-                });
-        })(i);
-    };
+	var names = ['#floatMenu'];
+	for (var i = 0; i < names.length; i++){
+		(function(i){
+			$(window).scroll(function(){
+				var menuYloc = $('.row-fluid').position().top+$('.row-fluid').outerHeight(true) + 30;
+				var offset = $(document).scrollTop() + 20 + 'px';
+				if ($(document).scrollTop() > menuYloc){
+					$(names[i]).animate({top:offset},{duration:0,queue:false});}
+				});
+		})(i);
+	};
 };
